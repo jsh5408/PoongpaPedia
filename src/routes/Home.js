7aea37c3f3518467d8f93a243b1fcf8a06549b11
Jun Seung Hyun <jsh5408@naver.com>
2021-12-12 15:20:54 +0900

    Update Home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,35 @@
+import {useState, useEffect} from "react";
+import styles from "./Home.module.css";
+
 function Home() {
+    const [movies, setMovies] = useState([]);
+
+    const getMovie = async () => {
+        const json = await (await fetch(`${process.env.REACT_APP_MOVIE_API}`)).json();
+        setMovies(json.results);
+        console.log(json.results);
+    }
+
+    useEffect(() => {
+        getMovie();
+    }, []);
+
     return (
         <div>
-            Home
+            <h1>Home</h1>
+            {
+                movies.map((movie, index) => (
+                    <div key={index}>
+                        <img
+                            className={styles.CoverImg}
+                            src={`${process.env.REACT_APP_IMAGE_URL}${movie.poster_path}`}
+                            alt={movie.title}
+                        />
+                        <h2>{movie.title}</h2>
+                        <p>{movie.overview}</p>
+                    </div>
+                ))
+            }
         </div>
     );
 }
